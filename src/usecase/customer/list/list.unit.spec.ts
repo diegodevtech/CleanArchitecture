26dcc512c0442b3ba6a18f4205e4c84a098ac148
diegodevtech <diegodevtechnology@gmail.com>
2025-01-15 14:15:18 -0400

    feat: finishes up list customer use case implementation

--- src/usecase/customer/list/list.unit.spec.ts
+++ src/usecase/customer/list/list.unit.spec.ts
@@ -1,5 +1,6 @@
 import CustomerFactory from "../../../domain/customer/factory/customer.factory";
 import Address from "../../../domain/customer/value-object/address";
+import ListCustomerUseCase from "./list";
 
 const customer1 = CustomerFactory.createWithAddress("Customer 1", new Address(
   "Rua",
@@ -30,16 +31,16 @@ describe("List Customers use case unit tests", () => {
     const listCustomerUseCase = new ListCustomerUseCase(customerRepository);
 
     // não mandamos parametro de entrada para execute, como num getAll()
-    const output = listCustomerUseCase.execute()
+    const output = await listCustomerUseCase.execute({})
 
     expect(output.customers.length).toBe(2);
 
     expect(output.customers[0].id).toBe(customer1.id);
     expect(output.customers[0].name).toBe(customer1.name);
-    expect(output.customers[0].street).toBe(customer1.street);
+    expect(output.customers[0].address.street).toBe(customer1.street);
 
     expect(output.customers[1].id).toBe(customer2.id);
     expect(output.customers[1].name).toBe(customer2.name);
-    expect(output.customers[1].street).toBe(customer2.street);
+    expect(output.customers[1].address.street).toBe(customer2.street);
   })
 })
