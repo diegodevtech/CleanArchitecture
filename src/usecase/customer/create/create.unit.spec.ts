5f0c99f10fe6371b88a1296eb7557470cf809d7c
diegodevtech <diegodevtechnology@gmail.com>
2025-01-15 11:59:00 -0400

    feat: adds create customer use case unit tests

---
+++ src/usecase/customer/create/create.unit.spec.ts
@@ -0,0 +1,39 @@
+const input = {
+  name: "Diego",
+  address: {
+    street: "Rua",
+    number: 1,
+    zip: "Zip",
+    city: "City",
+  }
+}
+
+const MockRepository = () => {
+  return {
+    find: jest.fn(),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  }
+}
+
+describe("Create customer use case unit tests", () => {
+  it("should create a customer", async () => {
+    const customerRepository = MockRepository();
+    const createCustomerUseCase = new CreateCustomerUseCase(customerRepository);
+
+    const output = await createCustomerUseCase.execute(input);
+
+    expect(output).toBe({
+      id: expect.any(String),
+      name: input.name,
+      address: {
+        street: input.address.street,
+        number: input.address.number,
+        zip: input.address.zip,
+        city: input.address.city,
+      }
+    });
+  });
+  
+});
