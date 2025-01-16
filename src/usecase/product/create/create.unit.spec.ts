173f0640e942adca6df4268bdf7e831fbe335e1e
diegodevtech <diegodevtechnology@gmail.com>
2025-01-16 11:29:28 -0400

    feat: adds create product use case unit tests

---
+++ src/usecase/product/create/create.unit.spec.ts
@@ -0,0 +1,30 @@
+const input = {
+  name: "Product A",
+  price: 20
+}
+
+const MockRepository = () => {
+  return {
+    create: jest.fn(),
+    update: jest.fn(),
+    findAll: jest.fn(),
+    find: jest.fn(),
+  };
+}
+
+describe("Create product use case unit tests", () => {
+  it("should create a product", async () => {
+    const productRepository = MockRepository();
+    const createProductUseCase = new CreateProductUseCase(productRepository);
+
+    const output = await createProductUseCase.execute(input);
+
+    expect(output).toStrictEqual({
+      id: expect.any(String),
+      name: input.name,
+      price: input.price,
+    });
+    
+  });
+
+})
